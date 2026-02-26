eb6ff30df3052c86091b3fc49afec3fa4ee7d096
chan <ckstj8027@naver.com>
2026-02-26 18:29:34 +0900

    fix: view error2

---
+++ server/src/main/java/com/k8s/cnapp/server/config/WebConfig.java
@@ -0,0 +1,25 @@
+package com.k8s.cnapp.server.config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new HandlerInterceptor() {
+            @Override
+            public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+                if (modelAndView != null) {
+                    modelAndView.addObject("requestURI", request.getRequestURI());
+                }
+            }
+        });
+    }
+}
